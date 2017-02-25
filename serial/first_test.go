d73be0353bbcd87fad5d7cc14d5ea8712051bf4c
Basile Starynkevitch <basile@starynkevitch.net>
2017-02-25 13:14:53 +0100

    improve first_test in serial

--- serial/first_test.go
+++ serial/first_test.go
@@ -9,5 +9,6 @@ import (
 
 func TestSerialToString(t *testing.T) {
 	s1 := SerialMo(2734358116516558954) // _3fZo81e6aIa
-	fmt.Printf("s1=%d:%s\n", s1, s1.ToString())
+	fmt.Printf("s1=%d\n", s1)
+	fmt.Printf("s1:%s\n", s1.ToString())
 }
