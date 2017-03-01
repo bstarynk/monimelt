6742c1505360130c4d9736d9d67c392b2b2222a6
Basile Starynkevitch <basile@starynkevitch.net>
2017-03-01 09:44:53 +0100

    adding TestMakeObjs

--- src/objvalmo/obj_test.go
+++ src/objvalmo/obj_test.go
@@ -14,3 +14,10 @@ func TestFirstObj(t *testing.T) {
 		FindObjectById(serialmo.IdFromCheckedSerials((serialmo.SerialMo)(0),
 			(serialmo.SerialMo)(0))))
 }
+
+func TestMakeObjs(t *testing.T) {
+	fmt.Printf("TestMakeObjs start\n")
+	ob1 := objvalmo.NewObj()
+	ob2 := objvalmo.NewObj()
+	fmt.Printf("ob1=%v ob2=%v\n", ob1, ob2)
+}
