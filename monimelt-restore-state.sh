97a8ba02cfb970cdaebdba74572e4df2cb9173ae
Basile Starynkevitch <basile@starynkevitch.net>
2017-03-09 19:21:34 +0100

    ob_mtime is an INT

---
+++ monimelt-restore-state.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+##   Copyright (C) 2017  Basile Starynkevitch
+##   This file is part of MONIMELT.
+## 
+##   MONIMELT is free software; you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation; either version 3, or (at your option)
+##   any later version.
+## 
+##   MONIMELT is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##   You should have received a copy of the GNU General Public License
+##   along with MONIMELT; see the file COPYING3.   If not see
+##   <http://www.gnu.org/licenses/>.
+
+## Dont change the name monimelt-restore-state.sh of this script without
+## care, it appears elsewhere
+
+echo start $0 "$@"
+dbfile=$1
+sqlfile=$2
+
+if [ -z "$dbfile" ] ; then
+    dbfile=monimelt_state.sqlite
+    sqlfile=monimelt_state.sql
+fi
+
+if [ -f "$dbfile" ] ; then
+    mv -v --backup "$dbfile" "$dbfile"
+fi
+sqlite3 "$dbfile" <  "$sqlfile"
+touch -r "$sqlfile" "$dbfile"
