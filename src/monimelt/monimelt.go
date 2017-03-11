3f9994acf59617a3c276eb9cbf897374c7870bc8
Basile Starynkevitch <basile@starynkevitch.net>
2017-03-11 08:30:44 +0100

    add -tiny-dump1 program option to run DoTinyDump1

--- src/monimelt/monimelt.go
+++ src/monimelt/monimelt.go
@@ -15,6 +15,7 @@ func main() {
 	hasSerialPtr := flag.Bool("serial", false, "generate serials and obids")
 	nbSerialPtr := flag.Int("nb-serial", 3, "number of serials")
 	loadPtr := flag.String("load", "", "initial load directory")
+	tinyDump1Ptr := flag.String("tiny-dump1", "", "directory to dump with DoTinyDump1")
 	finalDumpPtr := flag.String("final-dump", "", "final dump directory")
 	flag.Parse()
 	log.Printf("Monimelt starting pid %d, Go version %s\n", os.Getpid(), runtime.Version())
@@ -38,11 +39,20 @@ func main() {
 	if len(*loadPtr) > 0 {
 		log.Printf("monimelt should initial load from %s\n", *loadPtr)
 		objvalmo.LoadFromDirectory(*loadPtr)
+		log.Printf("monimelt did initial load from %s\n", *loadPtr)
+	}
+	//
+	if len(*tinyDump1Ptr) > 0 {
+		log.Printf("monimelt should dotinydump1 in %s\n", *tinyDump1Ptr)
+		objvalmo.DoTinyDump1(*tinyDump1Ptr)
+		log.Printf("monimelt did dotinydump1 in %s\n", *tinyDump1Ptr)
+
 	}
 	//
 	if len(*finalDumpPtr) > 0 {
 		log.Printf("monimelt should final dump in %s\n", *finalDumpPtr)
 		objvalmo.DumpIntoDirectory(*finalDumpPtr)
+		log.Printf("monimelt did final dump in %s\n", *finalDumpPtr)
 	}
 	log.Printf("Monimelt ending pid %d\n", os.Getpid())
 }
