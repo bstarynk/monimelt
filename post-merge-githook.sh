65366b75e7ce43d1eccd47727783d806aa049351
Basile Starynkevitch <basile@starynkevitch.net>
2017-03-13 19:45:40 +0100

    improve post-merge-githook.sh

--- post-merge-githook.sh
+++ post-merge-githook.sh
@@ -2,5 +2,5 @@
 # file post-merge-githook.sh to be installed as a git hook in .git/hooks/post-merge
 # also invoked thru git pull
 echo start post-merge-githook.sh "$@"
-./monimelt-restore-state.sh  monimelt_state.sqlite
+./monimelt-restore-state.sh  monimelt_state.sqlite monimelt_state.sql
 echo end post-merge-githook.sh "$@"
